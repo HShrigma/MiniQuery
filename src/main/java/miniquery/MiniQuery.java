97244c36a34382b6562e834b5074bc17a38afab7
HShrigma <hristostantchev@abv.bg>
2025-04-23 11:37:01 +0300

    chore(init): set up project structure with Maven and base Java file
    - Add pom.xml for build, packaging, and JUnit testing
    - Include basic directory structure
    - Add initial MiniQuery.java as main entry point

---
+++ src/main/java/miniquery/MiniQuery.java
@@ -0,0 +1,8 @@
+package miniquery;
+
+public class MiniQuery {
+
+    public static void main(String[] args) {
+        System.out.println("project is running");
+    }
+}
---
+++ src/test/java/miniquery/MiniQueryTest.java
@@ -0,0 +1,6 @@
+package miniquery;
+
+public class MiniQueryTest {
+
+    
+}
